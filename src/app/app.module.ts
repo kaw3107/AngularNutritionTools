929d73479134c051da35586d21fda9e879ca7edb
kieransmacbook <kieranalexwrenn@gmail.com>
2018-09-28 12:25:33 +0100

    Commit 280918 12:25 removed components and simplified user Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,6 @@ import { NavigationHomeComponent } from 'src/app/app-navigation/navigation-home/
 import { SharedModule } from './shared/shared.module';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { UserProfileComponent } from './user-dashboard/user-profile/user-profile.component';
-import { UserGoalsComponent } from './user-dashboard/user-goals/user-goals.component';
 import { SignUpDetailsComponent } from './auth/sign-up/sign-up-details/sign-up-details.component';
 import { FeedCardComponent } from './blog/feed-card/feed-card.component';
 import { CardioTrackerComponent } from './tracking-tools/cardio-tracker/cardio-tracker.component';
@@ -49,7 +48,6 @@ import { TrackingToolsComponent } from './tracking-tools/tracking-tools.componen
     NavigationHomeComponent,
     UserDashboardComponent,
     UserProfileComponent,
-    UserGoalsComponent,
     SignUpDetailsComponent,
     FeedCardComponent,
     StripHtmlTagsPipe,
