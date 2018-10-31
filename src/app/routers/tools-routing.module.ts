32a5fdd555a585a9e541963dc89f644e13903e6c
kieransmacbook <kieranalexwrenn@gmail.com>
2018-10-31 22:32:32 +0000

    commit

--- src/app/routers/tools-routing.module.ts
+++ src/app/routers/tools-routing.module.ts
@@ -6,12 +6,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { AuthGuard } from './../auth/auth.guard';
 import { TdeeCalculatorComponent } from '../tools/tdee-calculator/tdee-calculator.component';
+import { CardioTrackingComponent } from 'src/app/tools/cardio-tracking/cardio-tracking.component';
+
 
 export const routes: Routes = [
   { path: 'calories-calculator', component: CalorieCalculatorComponent},
   { path: 'macro-calculator', component: MacroCalculatorComponent},
   { path: 'bmr-calculator', component: BMRCalculatorComponent},
-  { path: 'tdee-calculator', component: TdeeCalculatorComponent}
+  { path: 'tdee-calculator', component: TdeeCalculatorComponent},
+  { path: 'cardio-tracker', component: CardioTrackingComponent },
 ];
 
 export const appRoutingProviders: any[] = [
